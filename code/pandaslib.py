9b387b1171fad76346280a7be947466447a07407
ojlangsyr <ojlang@syr.edu>
2024-10-05 12:33:17 -0400

    pandaslib and started unibrow

--- code/pandaslib.py
+++ code/pandaslib.py
@@ -5,11 +5,7 @@ SOLUTION FILE!!!!
 import pandas as pd
 
 def get_column_names(df : pd.DataFrame) -> list[str]:
-    '''
-    Get all column names of a pandas dataframe df
-    Returns the names as a list of string
-    '''
-    pass # todo: replace this line and add your code
+    return list(df.columns)
 
 
 def get_columns_of_type(df : pd.DataFrame, numpy_type: any) -> list[str]:
@@ -17,14 +13,20 @@ def get_columns_of_type(df : pd.DataFrame, numpy_type: any) -> list[str]:
     Return the column names of a pandas dataframe only when 
     the values in the column match the numpy_type
     '''
-    pass # todo: replace this line and add your code
-
+    if numpy_type == 'object':
+        return list(df.select_dtypes(include=[numpy_type]).columns)
+    elif numpy_type == 'int64':
+        return list(df.select_dtypes(include=[numpy_type]).columns)
+    elif numpy_type == 'float64':
+        return list(df.select_dtypes(include=[numpy_type]).columns)
+    else:
+        return []
 
 def get_unique_values(df : pd.DataFrame, column_name: str) -> list:
     '''
     Get a list of unique values of a column in a pandas dataframe
     '''
-    pass # todo: replace this line and add your code
+    return list(df[column_name].unique())
 
 def get_file_extension(file_path : str) -> str:
     '''
@@ -34,7 +36,7 @@ def get_file_extension(file_path : str) -> str:
     'countries.json' -> 'json'
 
     '''
-    pass # todo: replace this line and add your code
+    return file_path.split('.')[-1]
 
 def load_file(file_path: str, ext: str) -> pd.DataFrame:
     '''
@@ -44,7 +46,14 @@ def load_file(file_path: str, ext: str) -> pd.DataFrame:
     - when csv assume first row is header
     - when json assume record-oriented data
     '''
-    pass # todo: replace this line and add your code
+    if ext == 'csv':
+        return pd.read_csv(file_path)
+    elif ext == 'xlsx':
+        return pd.read_excel(file_path)
+    elif ext == 'json':
+        return pd.read_json(file_path)
+    else:
+        raise ValueError(f"Unsupported file extension: {ext}")
 
 if __name__ == '__main__':
     df = pd.DataFrame({ 
--- code/unibrow.py
+++ code/unibrow.py
@@ -5,8 +5,31 @@ import pandas as pd
 import streamlit as st
 import pandaslib as pl
 
-st.title("UniBrow")
-st.caption("The Universal data browser")
+
+st.title("UNIBROW")
+st.caption("THE UNIVERSAL DATA BROWSER")
 
 # TODO Write code here to complete the unibrow.py
 
+file_uploaded = st.file_uploader("UPLOAD A FILE (ONLY CSV, JSON OR XLSX)", type=["csv", "xlsx","json"])
+
+if file_uploaded:
+    file_ext = pl.get_file_extension(file_uploaded.name)
+    df = pl.load_file(file_uploaded, file_ext)
+
+    if df is not None:
+        all_cols = pl.get_column_names(df)
+        selected_cols = st.multiselect("Select columns", all_cols)
+
+        st.write("FILTERED:")
+        st.dataframe(df[selected_cols])
+
+        apply_filter = st.toggle("Apply filter", False)
+        if apply_filter:
+            text_cols = pl.get_columns_of_type(df, 'object')
+            if text_cols:
+                selected_text_col = st.selectbox("Select column filter", text_cols)
+                unique_values = pl.get_unique_values(df, selected_text_col)
+                selected_value = st.selectbox("Select value", unique_values)
+                st.write("FILTERED:")
+                st.dataframe(df[df[selected_text_col] == selected_value])
